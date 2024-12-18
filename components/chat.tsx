3fd944b009c0f2dbfeb59c66bf8a396dc11b6bd2
zt199510 <47783807+zt199510@users.noreply.github.com>
2024-12-18 02:11:02 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/chat.tsx
@@ -0,0 +1,147 @@
+'use client';
+
+import type { Attachment, Message } from 'ai';
+import { useChat } from 'ai/react';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
+import { useWindowSize } from 'usehooks-ts';
+
+import { ChatHeader } from '@/components/chat-header';
+import type { Vote } from '@/lib/db/schema';
+import { fetcher } from '@/lib/utils';
+
+import { Block, type UIBlock } from './block';
+import { BlockStreamHandler } from './block-stream-handler';
+import { MultimodalInput } from './multimodal-input';
+import { Messages } from './messages';
+import { VisibilityType } from './visibility-selector';
+
+export function Chat({
+  id,
+  initialMessages,
+  selectedModelId,
+  selectedVisibilityType,
+  isReadonly,
+}: {
+  id: string;
+  initialMessages: Array<Message>;
+  selectedModelId: string;
+  selectedVisibilityType: VisibilityType;
+  isReadonly: boolean;
+}) {
+  const { mutate } = useSWRConfig();
+
+  const {
+    messages,
+    setMessages,
+    handleSubmit,
+    input,
+    setInput,
+    append,
+    isLoading,
+    stop,
+    reload,
+    data: streamingData,
+  } = useChat({
+    id,
+    body: { id, modelId: selectedModelId },
+    initialMessages,
+    onFinish: () => {
+      mutate('/api/history');
+    },
+  });
+
+  const { width: windowWidth = 1920, height: windowHeight = 1080 } =
+    useWindowSize();
+
+  const [block, setBlock] = useState<UIBlock>({
+    documentId: 'init',
+    content: '',
+    kind: 'text',
+    title: '',
+    status: 'idle',
+    isVisible: false,
+    boundingBox: {
+      top: windowHeight / 4,
+      left: windowWidth / 4,
+      width: 250,
+      height: 50,
+    },
+  });
+
+  const { data: votes } = useSWR<Array<Vote>>(
+    `/api/vote?chatId=${id}`,
+    fetcher,
+  );
+
+  const [attachments, setAttachments] = useState<Array<Attachment>>([]);
+
+  return (
+    <>
+      <div className="flex flex-col min-w-0 h-dvh bg-background">
+        <ChatHeader
+          chatId={id}
+          selectedModelId={selectedModelId}
+          selectedVisibilityType={selectedVisibilityType}
+          isReadonly={isReadonly}
+        />
+
+        <Messages
+          chatId={id}
+          block={block}
+          setBlock={setBlock}
+          isLoading={isLoading}
+          votes={votes}
+          messages={messages}
+          setMessages={setMessages}
+          reload={reload}
+          isReadonly={isReadonly}
+        />
+
+        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+          {!isReadonly && (
+            <MultimodalInput
+              chatId={id}
+              input={input}
+              setInput={setInput}
+              handleSubmit={handleSubmit}
+              isLoading={isLoading}
+              stop={stop}
+              attachments={attachments}
+              setAttachments={setAttachments}
+              messages={messages}
+              setMessages={setMessages}
+              append={append}
+            />
+          )}
+        </form>
+      </div>
+
+      <AnimatePresence>
+        {block?.isVisible && (
+          <Block
+            chatId={id}
+            input={input}
+            setInput={setInput}
+            handleSubmit={handleSubmit}
+            isLoading={isLoading}
+            stop={stop}
+            attachments={attachments}
+            setAttachments={setAttachments}
+            append={append}
+            block={block}
+            setBlock={setBlock}
+            messages={messages}
+            setMessages={setMessages}
+            reload={reload}
+            votes={votes}
+            isReadonly={isReadonly}
+          />
+        )}
+      </AnimatePresence>
+
+      <BlockStreamHandler streamingData={streamingData} setBlock={setBlock} />
+    </>
+  );
+}
---
+++ components/messages.tsx
@@ -0,0 +1,85 @@
+import { ChatRequestOptions, Message } from 'ai';
+import { PreviewMessage, ThinkingMessage } from './message';
+import { useScrollToBottom } from './use-scroll-to-bottom';
+import { Overview } from './overview';
+import { UIBlock } from './block';
+import { Dispatch, memo, SetStateAction } from 'react';
+import { Vote } from '@/lib/db/schema';
+import equal from 'fast-deep-equal';
+
+interface MessagesProps {
+  chatId: string;
+  block: UIBlock;
+  setBlock: Dispatch<SetStateAction<UIBlock>>;
+  isLoading: boolean;
+  votes: Array<Vote> | undefined;
+  messages: Array<Message>;
+  setMessages: (
+    messages: Message[] | ((messages: Message[]) => Message[]),
+  ) => void;
+  reload: (
+    chatRequestOptions?: ChatRequestOptions,
+  ) => Promise<string | null | undefined>;
+  isReadonly: boolean;
+}
+
+function PureMessages({
+  chatId,
+  block,
+  setBlock,
+  isLoading,
+  votes,
+  messages,
+  setMessages,
+  reload,
+  isReadonly,
+}: MessagesProps) {
+  const [messagesContainerRef, messagesEndRef] =
+    useScrollToBottom<HTMLDivElement>();
+
+  return (
+    <div
+      ref={messagesContainerRef}
+      className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
+    >
+      {messages.length === 0 && <Overview />}
+
+      {messages.map((message, index) => (
+        <PreviewMessage
+          key={message.id}
+          chatId={chatId}
+          message={message}
+          block={block}
+          setBlock={setBlock}
+          isLoading={isLoading && messages.length - 1 === index}
+          vote={
+            votes
+              ? votes.find((vote) => vote.messageId === message.id)
+              : undefined
+          }
+          setMessages={setMessages}
+          reload={reload}
+          isReadonly={isReadonly}
+        />
+      ))}
+
+      {isLoading &&
+        messages.length > 0 &&
+        messages[messages.length - 1].role === 'user' && <ThinkingMessage />}
+
+      <div
+        ref={messagesEndRef}
+        className="shrink-0 min-w-[24px] min-h-[24px]"
+      />
+    </div>
+  );
+}
+
+export const Messages = memo(PureMessages, (prevProps, nextProps) => {
+  if (prevProps.isLoading !== nextProps.isLoading) return false;
+  if (prevProps.isLoading && nextProps.isLoading) return false;
+  if (prevProps.messages.length !== nextProps.messages.length) return false;
+  if (!equal(prevProps.votes, nextProps.votes)) return false;
+
+  return true;
+});
